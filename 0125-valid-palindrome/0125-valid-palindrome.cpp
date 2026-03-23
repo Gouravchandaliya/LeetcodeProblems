ade34d79a7570d42ac5b07ad0634fc1bfeb7f314
Gouravchandaliya <jaingourav1020@gmail.com>
2026-03-23 20:56:17 +0530

    Time: 0 ms (100%), Space: 10.2 MB (23.75%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp = "";
+        for (int i = 0; i < s.length(); i++) {
+            if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') ||
+                (s[i] >= '0' && s[i] <= '9')) {
+                temp.push_back(s[i]);
+            }
+        }
+        for (int j = 0; j < temp.length(); j++) {
+            if (temp[j] >= 'A' && temp[j] <= 'Z') {
+                temp[j] = temp[j] - 'A' + 'a';
+            }
+        }
+        int st = 0;
+        int e = temp.length() - 1;
+        while (st <= e) {
+            if (temp[st] != temp[e]) {
+                return false;
+            } else {
+                st++;
+                e--;
+            }
+        }
+        return true;
+    }
+};
